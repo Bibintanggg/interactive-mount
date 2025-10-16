b23c7cb85fad771528962150f32febb84f98415c
Bibintanggg <bintangyudhaa8@gmail.com>
2025-10-17 00:26:17 +0700

    feat: added sidebar

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+// import { Sidebar } from "@/components/ui/sidebar";
+import { AppSidebar } from "../Components/app-sidebar";
+
+export default function Home() {
+    return (
+        <div>
+            <AppSidebar/>
+        </div>
+    )
+}
